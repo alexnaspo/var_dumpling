52fd987b492e0cb0fe2d3cc6d56839b5c7e02445
Alexander Naspo <alex.naspo@gmail.com>
2012-10-12 00:09:24 -0400

    now detects assossiated arrays

--- var_dumpling.js
+++ var_dumpling.js
@@ -1,26 +1,27 @@
 if ($('body').children().length == 0) {
 
   var dumpling = $('body').text();
-  var startingArray = dumpling.match(/^array\([0-9]*\)\s\{/g);
+  var startingArray = dumpling.match(/^array\([0-9]*\)\s\{|^array\s\(/g);
+  
   if (startingArray) {
 
     dumpling = dumpling.replace(/(\n\s\s|\n)/gm, " ");
     dumpling = dumpling.replace(/([\s]+)/gm, " ");
-    var myArray = dumpling.match(/\["?[a-zA-Z0-9\.]*"?]=> [a-z]*\([0-9\.]*\)\s(\{)?("[a-zA-z]*")?|\}/g);
+    var oldArray = dumpling.match(/\["?[a-zA-Z0-9\.]*"?]=> [a-z]*\([0-9\.]*\)\s(\{)?("[a-zA-z]*")?|\}/g);
 
-    // someText = dumpling.replace(/(\r\n|\n|\r|\s\s)/gm,".");
+    var oldArray = dumpling.match(/\'[@a-zA-Z0-9_-]*\'\s\=>\s(\'?[@a-zA-Z0-9_ -:]*\'\,|NULL)|\),?|\["?[a-zA-Z0-9\.]*"?]=> [a-z]*\([0-9\.]*\)\s(\{)?("[a-zA-z]*")?|\}/g);
 
-    var startingArray = dumpling.match(/^array\([0-9]*\)\s\{/g);
+    var myArray = dumpling.match(/\'[@a-zA-Z0-9_-]*\'\s\=>\s(\'?[@a-zA-Z0-9_ -:]*\'\,|NULL)|\s\),?|\["?[a-zA-Z0-9\.]*"?]=> [a-z]*\([0-9\.]*\)\s(\{)?("[a-zA-z]*")?|\}|[0-9]*\s=>\s[a-zA-Z]*\s\(/g);
 
     $('body').empty();
     var nestLevel = 1;
     $('body').append("<div class='header'>" + startingArray.toString() + "</div>");
     for (i = 0; i < myArray.length; i++) {
-      if (myArray[i] == "}") {
+      if (myArray[i] == "}" || myArray[i] == " )," || myArray[i] == " )") {
         nestLevel--;
       }
       $('body').append("<div class='element' id=nestLevel" + nestLevel + ">" + myArray[i].toString() + "</div>");
-      if (myArray[i].match(/array\([0-9]*\)\s\{/)) {
+      if (myArray[i].match(/array\([0-9]*\)\s\{|array\s\(|array\s=>/)) {
         nestLevel++;
       }
     }
