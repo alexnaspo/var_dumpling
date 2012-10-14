71f94502eedee7c1e01b7dcc7eaadbac87e7d96d
Alexander Naspo <alex.naspo@gmail.com>
2012-10-14 00:29:38 -0400

    regex fix

--- var_dumpling.js
+++ var_dumpling.js
@@ -7,7 +7,8 @@ if ($('body').children().length == 0) {
 
     dumpling = dumpling.replace(/(\n\s\s|\n)/gm, " ");
     dumpling = dumpling.replace(/([\s]+)/gm, " ");
-    var myArray = dumpling.match(/array\([0-9]*\)\s\{|\'[@a-zA-Z0-9_-]*\'\s\=>\s(\'?[@a-zA-Z0-9_ -:]*\'\,|NULL)|\s\),?|\["?[a-zA-Z0-9\.]*"?]=> [a-z]*\([0-9\.]*\)\s(\{)?("[a-zA-z]*")?|\}|[0-9]*\s=>\s[a-zA-Z]*\s\(/g);
+    console.log(dumpling);
+    var myArray = dumpling.match(/array\([0-9]*\)\s\{|\'[@a-zA-Z0-9_-]*\'\s\=>\s(\'?[@a-zA-Z0-9_ -:]*\'\,|NULL)|\s\),?|\["?[a-zA-Z0-9_\.\/]*"?]=>\s([a-zA-Z]*\([0-9a-zA-Z\.]*\)\s(\{)?("[0-9a-zA-Z\/]*")?|NULL)|\}|[0-9]*\s=>\s[a-zA-Z]*\s\(/g);
 
     $('body').empty();
     var nestLevel = 0;
@@ -20,7 +21,7 @@ if ($('body').children().length == 0) {
       myArray[i] = highlight(myArray[i]);
       $('body').append("<div class='element nestLevel" + nestLevel + "' id='" + i + "'>" + myArray[i].toString() + "</div>");
 
-      if (myArray[i].match(/array\([0-9]*\)\s\{|array\s\(|array\s=>/)) {
+      if (myArray[i].match(/array\([0-9]*\)\s\{|array\s\(|array\s=>|\s\{/)) {
         nestLevel++;
       }
     }
@@ -29,22 +30,31 @@ if ($('body').children().length == 0) {
 
 function highlight (element) {
 
-  if(element.toString().match(/\["?[0-9a-zA-Z]*"?\]=>/i)){
-    var index = element.toString().match(/\["?[0-9a-zA-Z]*"?\]/i);
-    element = element.toString().replace(/\["?[0-9a-zA-Z]*"?\]/i, "<span class='index'>" + index + "</span>");
+  if(element.toString().match(/\["?.*"?\]=>/i)){
+    var index = element.toString().match(/\["?.*"?\]/i);
+    element = element.toString().replace(/\["?.*"?\]/i, "<span class='index'>" + index + "</span>");
   }
 
-  if(element.toString().match(/\sint\([0-9]*\)/i)){
-    var integer = element.toString().match(/\([0-9]*\)/i);
-    console.log(integer);
+  if(element.toString().match(/\s(int|float)\([0-9.]+\)/i)){
+    var integer = element.toString().match(/\([0-9.]+\)/i);
     integer = integer.toString().replace(/\(|\)/g, ' ');
-    element = element.toString().replace(/int\([0-9]*\)/i, "<span class='int'>" + integer + "</span>");
+    element = element.toString().replace(/(int|float)\([0-9.]+\)/i, "<span class='int'>" + integer + "</span>");
     return element;
   }
 
-  if(element.toString().match(/\s"[a-z]*"/i)){
-    var str = element.toString().match(/\s"[a-z]*"/i);
-    element = element.toString().replace(/string\([0-9*\)\s"[a-z]*"/i, "<span class='string'>" + str + "</span>");
+  if(element.toString().match(/array\([0-9]*\)\s\{/i)){
+    console.log('here');
+    var array = element.toString().match(/array\([0-9]*\)\s\{/i);
+    array = array.toString().replace(/array\([0-9]*\)/i, ' ');
+
+    element = element.toString().replace(/array\([0-9]*\)\s\{/i, "<span class='array'>" + array + "</span>");
+    return element;
+  }
+
+
+  if(element.toString().match(/\s".*"/i)){
+    var str = element.toString().match(/\s".*"/i);
+    element = element.toString().replace(/string\([0-9]*\)\s".*"/i, "<span class='string'>" + str + "</span>");
     return element;
   } else {
     return element;
