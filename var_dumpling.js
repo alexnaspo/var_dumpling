0969ea628630a7f7f27ed6afdf4f78966850a1f0
Alexander Naspo <alex.naspo@gmail.com>
2012-10-21 20:51:24 -0400

    added nesting support for objects
    css padding classes removed
    padding now added through jquery

--- var_dumpling.js
+++ var_dumpling.js
@@ -1,39 +1,42 @@
-if ($('body').children().length == 0) {
-
+//if ($('body').children().length == 0) {
   var dumpling = $('body').text();
-  var starting = dumpling.match(/[objectarray]*\([0-9a-zA-Z]*\)(#[0-9]*)?\s(\([0-9]*\)\s)?\{/g);
+  var starting = dumpling.match(/^(\[[0-9a-zA-Z"]*\]=>\s)*?[objectarray]*\([0-9a-zA-Z\\]*\)(#[0-9]*)?\s(\([0-9]*\)\s)?\{/g);
   
   if(starting) {
-
     dumpling = dumpling.replace(/(\n\s\s|\n)/gm, " ");
     dumpling = dumpling.replace(/([\s]+)/gm, " ");
-    console.log(dumpling);
-    var myArray = dumpling.match(/array\([0-9]*\)\s\{|\'[@a-zA-Z0-9_-]*\'\s\=>\s(\'?[@a-zA-Z0-9_ -:]*\'\,|NULL)|\s\),?|\["?[a-zA-Z0-9_\.\/]*"?]=>\s([a-zA-Z]*\([0-9a-zA-Z\.]*\)\s(\{)?("[0-9a-zA-Z\/]*")?|NULL)|\}|[0-9]*\s=>\s[a-zA-Z]*\s\(/g);
-    myArray = dumpling.match(/(\[[0-9]*\]=>\s)*?[objectarray]*\([0-9a-zA-Z]*\)(#[0-9]*)?\s(\([0-9]*\)\s)?\{|\["?[a-zA-Z0-9_\.\/]*"?]=>\s([a-zA-Z]*\([0-9a-zA-Z\.]*\)\s(\{)?(\"(.*?)\")?|NULL)|\}/g);
+    
+    var myArray = dumpling.match(/(\[[0-9]*\]=>\s)*?[objectarray]*\([0-9a-zA-Z]*\)(#[0-9]*)?\s(\([0-9]*\)\s)?\{|\["?[a-zA-Z0-9_\.\/]*"?]=>\s([a-zA-Z]*\([0-9a-zA-Z\.]*\)\s(\{)?(\"(.*?)\")?|NULL)|\}/g);
+    myArray = dumpling.match(/(\[[0-9a-zA-Z"]*\]=>\s)*?[objectarray]*\([0-9a-zA-Z\\]*\)(#[0-9]*)?\s(\([0-9]*\)\s)?\{|\["?[a-zA-Z0-9:"_\.\/]*"?]=>\s([a-zA-Z]*\([0-9a-zA-Z\.]*\)\s(\{)?(\"(.*?)\")?|NULL)|\}/g);
 
     $('body').empty();
     var nestLevel = 0;
-    //$('body').append("<div class='header'>" + startingArray.toString() + "</div>");
     for (i = 0; i < myArray.length; i++) {
       if (myArray[i] == "}" || myArray[i] == " )," || myArray[i] == " )") {
         nestLevel--;
       }
 
+      if (myArray[i].match(/(\[[0-9a-zA-Z"]*\]=>\s)*?[objectarray]*\([0-9a-zA-Z\\]*\)(#[0-9]*)?\s(\([0-9]*\)\s)?\{/)) {
+        nestLevel++;
+      }
+      
       myArray[i] = highlight(myArray[i]);
-      $('body').append("<div class='element nestLevel" + nestLevel + "' id='" + i + "'>" + myArray[i].toString() + "</div>");
-
-      if (myArray[i].match(/array\([0-9]*\)\s\{|array\s\(|array\s=>|\s\{/)) {
+      if(i == 0){
+        nestLevel = 0;
+      }
+      $('body').append("<div class='element nestLevel" + nestLevel + "' style='padding-left:" + (nestLevel * 10) +"px;'" + ">" + myArray[i].toString() + "</div>");
+      if(i == 0){
         nestLevel++;
       }
     }
   }
-}
+//}
 
 function highlight (element) {
 
   if(element.toString().match(/\["?.*"?\]=>/i)){
     var index = element.toString().match(/\["?.*"?\]/i);
-    element = element.toString().replace(/\["?.*"?\]/i, "<span class='index'>" + index + "</span>");
+    element = element.toString().replace(/\["?.*"?\]=>/i, "<span class='index'>" + index + "</span>");
   }
 
   if(element.toString().match(/\s(int|float)\([0-9.]+\)/i)){
@@ -60,7 +63,6 @@ function highlight (element) {
     element = element.toString().replace(/bool\([0-9a-zA-Z]*\)/i, "<span class='bool'>" + bool + "</span>");
     return element;
   }  
-
 
 
   if(element.toString().match(/\s".*"/i)){
