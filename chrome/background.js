5e246d0a409629ef29ca593e880aa90809756980
Alexander Naspo <alex.naspo@gmail.com>
2013-05-21 10:17:28 -0400

    created builds directory

--- chrome/background.js
+++ chrome/background.js
@@ -1,10 +1,14 @@
 chrome.browserAction.onClicked.addListener(function() { 
   chrome.browserAction.getBadgeText({},function(result){
     if(result == "off"){
-      chrome.browserAction.setBadgeText({text:''});  
+      chrome.browserAction.setBadgeText({text:''});
     } else {
       chrome.browserAction.setBadgeText({text:'off'}); 
     }
+    chrome.tabs.getSelected(null, function(tab) {
+      var code = 'window.location.reload();';
+      chrome.tabs.executeScript(tab.id, {code: code});
+    });
   });  
 });
 
@@ -12,7 +16,7 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo) {
   if (changeInfo.status === 'complete') {
     chrome.browserAction.getBadgeText({},function(result){
       if(result != "off"){
-        chrome.tabs.executeScript(null, {
+        chrome.tabs.executeScript(tabId, {
             file: "var_dumpling.js"
         });
       }
