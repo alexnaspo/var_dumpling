1a3a6946910ed1f546911b01085aec8f10639e1a
Alexander Naspo <alex.naspo@gmail.com>
2013-09-13 00:29:37 -0400

    fixes magneto var_dumps
    cleaned code and removed CSS

--- chrome/var_dumpling.js
+++ chrome/var_dumpling.js
@@ -1,81 +1,100 @@
 (function() { 
-  var dumpling = document.body.textContent;  
-  var starting = dumpling.match(/(\[[0-9a-zA-Z"]*\]=>\s)*?(?:object|array)\([0-9a-zA-Z\\]*\)(#[0-9]*)?\s(\([0-9]*\)\s)?\{/g);
-
-  if(starting) {
-    //var_dump detected
-    dumpling = dumpling.replace(/(\n\s\s|\n)/gm, " ");
-    dumpling = dumpling.replace(/([\s]+)/gm, " ");
-
-    var myArray = dumpling.match(/(\[[0-9a-zA-Z_"]*\]=>\s)*?(?:object|array)\([0-9a-zA-Z\\]*\)(#[0-9]*)?\s(\([0-9]*\)\s)?\{|\["?[a-zA-Z0-9:"_\.\/]*"?]=>\s([a-zA-Z]*\([0-9a-zA-Z\.]*\)\s(\{)?(\"(.*?)\")?|NULL)|\}/g);
-        
-    var nestLevel = 0;
-    var var_dumpling = "";
-
-    for (i = 0; i < myArray.length; i++) {
-      var ingredient = {
-        text: highlight(myArray[i]),
-      }
-      if (myArray[i].match(/(\[[0-9a-zA-Z"]*\]=>\s)*?(?:object|array)\([0-9a-zA-Z\\]*\)(#[0-9]*)?\s(\([0-9]*\)\s)?\{/)) {
-        ingredient.nestLevel = nestLevel;
-        nestLevel++;
-      } else if (myArray[i] == "}" || myArray[i] == " )," || myArray[i] == " )") {
-        nestLevel--;
-        ingredient.nestLevel = nestLevel;
-      } else {
-        ingredient.nestLevel = nestLevel;
-      }
-      
-      var_dumpling += "<div class='element nestLevel" + ingredient.nestLevel + "' style='padding-left:" + (ingredient.nestLevel * 15) +"px;'" + ">" + ingredient.text + "</div>";
-    }
-    if(nestLevel == 0) {
-      //nestLevel is balanced, var_dump confirmed
-      document.body.innerHTML = "<div id='var_dumpling'>" + var_dumpling + "</div";
-      // document.body.innerHTML += var_dumpling;
-      // document.body.innerHTML += "</div>";
-    } 
+  function main(){
+    var body = document.body.textContent;  
+    var dumpling = var_dumpling(body);
+    if(dumpling){
+      document.body.innerHTML = dumpling;  
+    }   
   }
 
-  function highlight (element) {
+  function var_dumpling(body){
+    var objectOrArray = /(\[[0-9a-zA-Z"]*\]=>\s)*?(?:object|array)\([0-9a-zA-Z_\\]*\)(#[0-9]*)?\s(\([0-9]*\)\s)?\{/g;  
+    var elementsRegex = /(\[[0-9a-zA-Z_"]*\]=>\s)*?(?:object|array)\([0-9a-zA-Z_\\]*\)(#[0-9]*)?\s(\([0-9]*\)\s)?\{|\["?[a-zA-Z0-9:"_\.\/]*"?]=>\s([a-zA-Z]*\([0-9a-zA-Z\.]*\)\s(\{)?(\"(.*?)\")?|NULL)|\}/g;
 
-    if(element.toString().match(/\["?.*"?\]=>/i)){
-      var index = element.toString().match(/\["?.*"?\]/i);
-      element = element.toString().replace(/\["?.*"?\]=>/i, "<span class='index'>" + index + "</span>");
+    var starting = body.match(objectOrArray);
+    if(starting) {
+      //var_dump detected
+      body = body.replace(/(\n\s\s|\n)/gm, " ");
+      body = body.replace(/([\s]+)/gm, " ");    
+
+      var elements = body.match(elementsRegex);          
+      var nestLevel = 0;
+      var var_dumpling = "";
+
+      for (i = 0; i < elements.length; i++) {      
+        ingredient = new Ingredient(elements[i]);
+        if (elements[i].match(objectOrArray)) {
+          ingredient.nestLevel = nestLevel;
+          nestLevel++;
+        } else if (elements[i] == "}" || elements[i] == " )," || elements[i] == " )") {
+          nestLevel--;
+          ingredient.nestLevel = nestLevel;
+        } else {
+          ingredient.nestLevel = nestLevel;
+        }        
+        var_dumpling += "<div style='padding-left:" + (ingredient.nestLevel * 15) +"px;'" + ">" + ingredient.text + "</div>";
+      }
+      if(nestLevel == 0) {
+        //nestLevel is balanced, var_dump confirmed
+        return "<div id='var_dumpling'>" + var_dumpling + "</div";
+      } else {
+        return false;
+      }
+    }
+  }
+
+  function Ingredient(element) {
+    this.text = highlight(element);
+    this.nestLevel = null;
+  } 
+
+  function highlight (element) {
+    var objectRegex = /[object]*\([a-zA-Z\\]*\)(#[0-9]*)\s(\([0-9]*\)\s)?\{/i;
+    var indexRegex = /\["?.*"?\]=>/i;
+    var arrayRegex = /array\([0-9]*\)\s\{/i;
+    var boolRegex = /bool\([0-9a-zA-Z]*\)/i; 
+    var stringRegex = /\s".*"/i;
+
+    if(element.match(indexRegex)){
+      var index = element.toString().match(indexRegex);
+      element = element.toString().replace(indexRegex, index);
     }
 
-    if(element.toString().match(/[object]*\([a-zA-Z\\]*\)(#[0-9]*)\s(\([0-9]*\)\s)?\{/i)){
-      var object =  element.toString().match(/[object]*\([a-zA-Z\\]*\)(#[0-9]*)\s(\([0-9]*\)\s)?\{/i);
-      element = element.toString().replace(/[object]*\([a-zA-Z\\]*\)(#[0-9]*)\s(\([0-9]*\)\s)?\{/i, "<span class='object'>" + object[0] + "</span>");
+    if(element.toString().match(objectRegex)){
+      var object =  element.toString().match(objectRegex);
+      element = element.toString().replace(objectRegex, object[0]);
       return element;
     }
+
+    if(element.toString().match(arrayRegex)){
+      var array = element.toString().match(arrayRegex);      
+      element = element.toString().replace(arrayRegex, array);
+      return element;
+    }
+
+    if(element.match(boolRegex)){
+      var bool = element.match(boolRegex);
+      bool = bool.toString().replace(/bool\(|\)/g, ' ');      
+      element = element.toString().replace(boolRegex, bool.fontcolor("#9A32CD"));
+      return element;
+    }  
 
     if(element.toString().match(/\s(int|float)\([0-9.]+\)/i)){
       var integer = element.toString().match(/\([0-9.]+\)/i);
       integer = integer.toString().replace(/\(|\)/g, ' ');
-      element = element.toString().replace(/(int|float)\([0-9.]+\)/i, "<span class='int'>" + integer + "</span>");
-      return element;
-    }
-
-    if(element.toString().match(/array\([0-9]*\)\s\{/i)){
-      var array = element.toString().match(/array\([0-9]*\)\s\{/i);      
-      element = element.toString().replace(/array\([0-9]*\)\s\{/i, "<span class='array'>" + array + "</span>");
-      return element;
-    }
-
-    if(element.toString().match(/bool\([0-9a-zA-Z]*\)/i)){
-      var bool = element.toString().match(/bool\([a-zA-Z0-9]*\)/i);
-      bool = bool.toString().replace(/bool\(|\)/g, ' ');
-      element = element.toString().replace(/bool\([0-9a-zA-Z]*\)/i, "<span class='bool'>" + bool + "</span>");
+      element = element.toString().replace(/(int|float)\([0-9.]+\)/i, integer.fontcolor("#900").bold());
       return element;
     }  
 
-    if(element.toString().match(/\s".*"/i)){
-      var str = element.toString().match(/\s".*"/i);
-      element = element.toString().replace(/string\([0-9]*\)\s".*"/i, "<span class='string'>" + str + "</span>");
+    if(element.toString().match(stringRegex)){
+      var str = element.toString().match(stringRegex);
+      element = element.toString().replace(/string\([0-9]*\)\s".*"/i, str.toString().fontcolor("#090"));
       return element;
     } else {
       return element;
     }
-
   }
+
+  main();
+  
 }());
